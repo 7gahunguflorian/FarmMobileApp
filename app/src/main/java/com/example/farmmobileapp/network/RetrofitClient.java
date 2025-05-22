b014ca34e9f777e764ca1fab55de34551ca954aa
7gahunguflorian <gahungufloriannosimbola@gmail.com>
2025-05-22 14:26:14 +0200

    connection with the api

--- app/src/main/java/com/example/farmmobileapp/network/RetrofitClient.java
+++ app/src/main/java/com/example/farmmobileapp/network/RetrofitClient.java
@@ -10,7 +10,12 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://localhost:8180/";
+    public static final String BASE_URL = "http://192.168.1.100:8180/";
+
+    // http://10.0.2.2:8180/ For Android emulator
+    // Use "http://localhost:8180/" if testing on physical device with backend on same network
+    // Use your actual IP address like "http://192.168.1.100:8180/" for physical device
+
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
